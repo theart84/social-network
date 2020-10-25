355ec828bb392696db177a04810507696417a730
theart84 <artshadow74@gmail.com>
2020-10-25 19:39:15 +0500

    Заменил функциональную компоненту Users на классовую компоненту.

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,29 +3,25 @@ import users from './Users.module.css'
 import * as axios from 'axios'
 import userAvatar from '../../../src/img/user.png';
 
-const Users = (props) => {
-  let getUsers = () => {
-    if (props.users.length === 0) {
-      axios.get('https://social-network.samuraijs.com/api/1.0/users')
-        .then(response => props.setUser(response.data.items))
-    }
+class Users extends React.Component {
+  constructor(props) {
+    super(props);
+    axios.get('https://social-network.samuraijs.com/api/1.0/users')
+      .then(response => this.props.setUser(response.data.items))
   }
-
-
-  return (
-    <div className={users.wrapper}>
+  render() {
+     return ( <div className={users.wrapper}>
       <h2 className={users.section_title}>Users</h2>
-      <input type="button" onClick={getUsers} value='Get Users'/>
       {
-        props.users.map(user => (
+        this.props.users.map(user => (
           <div key={user.id} className={users.wrapper_block}>
             <div className={users.avatar_button}>
               <div className={users.img}>
                 <img src={user.photos.small !== null ? user.photos.small : userAvatar} alt=""/>
               </div>
               <div className={users.btn}>
-                {user.followed ? <button onClick={() => props.unfollow(user.id)}>FOLLOW</button>
-                  : <button onClick={() => props.follow(user.id)}>UNFOLLOW</button>
+                {user.followed ? <button onClick={() => this.props.unfollow(user.id)}>FOLLOW</button>
+                  : <button onClick={() => this.props.follow(user.id)}>UNFOLLOW</button>
                 }
               </div>
             </div>
@@ -38,8 +34,8 @@ const Users = (props) => {
           </div>
         ))
       }
-    </div>
-  )
+    </div>)
+  }
 }
 export default Users;
 
