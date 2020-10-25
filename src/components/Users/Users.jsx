ac351d007162a7e8e54fe15ab3d28de2b1547f80
theart84 <artshadow74@gmail.com>
2020-10-25 18:50:59 +0500

    Добавил кнопку Get Users

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,26 +4,28 @@ import * as axios from 'axios'
 import userAvatar from '../../../src/img/user.png';
 
 const Users = (props) => {
-
-  if (props.users.length === 0) {
+  let getUsers = () => {
+    if (props.users.length === 0) {
       axios.get('https://social-network.samuraijs.com/api/1.0/users')
-           .then(response => props.setUser(response.data.items))
-
+        .then(response => props.setUser(response.data.items))
+    }
   }
+
 
   return (
     <div className={users.wrapper}>
       <h2 className={users.section_title}>Users</h2>
+      <input type="button" onClick={getUsers} value='Get Users'/>
       {
         props.users.map(user => (
-          <div className={users.wrapper_block}>
+          <div key={user.id} className={users.wrapper_block}>
             <div className={users.avatar_button}>
               <div className={users.img}>
                 <img src={user.photos.small !== null ? user.photos.small : userAvatar} alt=""/>
               </div>
               <div className={users.btn}>
                 {user.followed ? <button onClick={() => props.unfollow(user.id)}>FOLLOW</button>
-                               : <button onClick={() => props.follow(user.id)}>UNFOLLOW</button>
+                  : <button onClick={() => props.follow(user.id)}>UNFOLLOW</button>
                 }
               </div>
             </div>
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { connect } from 'react-redux'
 import {followAC, setUserAC, unfollowAC} from "../../redux/reducerUsers";
 import Users from "./Users";
